4fbac01c15cc7ad2094af242f16537497a5e2d65
Josh Brown <joshcarlbrown@gmail.com>
2025-02-03 10:38:18 +1100

    first go at introducing errors

--- app/AST.hs
+++ app/AST.hs
@@ -1,5 +1,7 @@
-module AST where
+module AST (BinOp (..), UnOp (..), Value (..), Expr_ (..), Expr, expr, SourcePos (..))
+where
 
+import Control.Applicative ((<**>))
 import Control.Monad.Combinators.Expr
 import Data.Map qualified as M
 import Data.Text qualified as T
@@ -14,15 +16,19 @@ data UnOp = Negate | Not
   deriving (Show)
 data Value = TNum Double | TString String | TBool Bool | TNil
   deriving (Show)
-data Expr
-  = Ident String
-  | Value Value
-  | UnOp UnOp Expr
-  | BinOp BinOp Expr Expr
+data Expr_ a
+  = Ident String a
+  | Value Value a
+  | UnOp UnOp Expr a
+  | BinOp BinOp Expr Expr a
   deriving (Show)
 
+type Expr = Expr_ SourcePos
+
 type Parser = Parsec Void T.Text
-type Assignments = M.Map T.Text Expr
+
+withLocation :: Parser (SourcePos -> Expr) -> Parser Expr
+withLocation p = getSourcePos <**> p
 
 sc :: Parser ()
 sc = L.space space1 (L.skipLineComment "//") (L.skipBlockComment "/*" "*/")
@@ -34,17 +40,18 @@ symbol :: T.Text -> Parser T.Text
 symbol = L.symbol sc
 
 var :: Parser Expr
-var = Ident <$> lexeme ((:) <$> letterChar <*> many alphaNumChar)
+var = withLocation $ Ident <$> lexeme ((:) <$> letterChar <*> many alphaNumChar)
 
 literal :: Parser Expr
 literal =
-  choice
-    [ Value . TNum <$> lexeme (try L.float <|> L.decimal)
-    , Value . TString <$> (char '\"' *> manyTill L.charLiteral (char '\"'))
-    , Value (TBool True) <$ symbol "true"
-    , Value (TBool False) <$ symbol "false"
-    , Value TNil <$ symbol "nil"
-    ]
+  withLocation $
+    choice
+      [ Value . TNum <$> lexeme (try L.float <|> L.decimal)
+      , Value . TString <$> lexeme (char '\"' *> manyTill L.charLiteral (char '\"'))
+      , Value (TBool True) <$ symbol "true"
+      , Value (TBool False) <$ symbol "false"
+      , Value TNil <$ symbol "nil"
+      ]
 
 parens :: Parser a -> Parser a
 parens = between (symbol "(") (symbol ")")
@@ -55,17 +62,19 @@ atom = choice [parens expr, literal, var]
 expr :: Parser Expr
 expr = makeExprParser atom operatorTable
 
-binary :: T.Text -> (Expr -> Expr -> Expr) -> Operator Parser Expr
-binary name f = InfixL (f <$ symbol name)
+binary :: T.Text -> (Expr -> Expr -> SourcePos -> Expr) -> Operator Parser Expr
+binary name f = InfixL (flip3 f <$> (getSourcePos <* symbol name))
 
-prefix, postfix :: T.Text -> (Expr -> Expr) -> Operator Parser Expr
-prefix name f = Prefix (f <$ symbol name)
-postfix name f = Postfix (f <$ symbol name)
+prefix :: T.Text -> (Expr -> SourcePos -> Expr) -> Operator Parser Expr
+prefix name f = Prefix (flip f <$> (getSourcePos <* symbol name))
+
+flip3 :: (a -> b -> c -> d) -> c -> a -> b -> d
+flip3 f c a b = f a b c
 
 -- decreasing precedence
 -- there is a tradeoff here with the library where multiple occurences of
 -- prefix operators don't work, e.g., !!false. it's pretty clear in the source
--- (https://hackage.haskell.org/package/parser-combinators-1.3.0/docs/src/Control.Monad.Combinators.Expr.html#pTerm)
+-- (https://hackage.haskell.org/package/parser-combinators-1.3.0/docs/src/Control.Monad.Combinators.Expr_.html#pTerm)
 -- where this arises, but i cbs rolling my own
 operatorTable :: [[Operator Parser Expr]]
 operatorTable =
@@ -73,7 +82,7 @@ operatorTable =
   ,
     [ prefix "-" (UnOp Negate)
     , prefix "!" (UnOp Not)
-    , prefix "+" id
+    , prefix "+" const
     ]
   ,
     [ binary "*" (BinOp Mul)
--- app/Eval.hs
+++ app/Eval.hs
@@ -1,52 +1,73 @@
 module Eval where
 
-import AST (BinOp (..), Expr (..), UnOp (..), Value (..))
+import AST (BinOp (..), Expr, Expr_ (..), SourcePos (..), UnOp (..), Value (..), unPos)
+import Control.Applicative ((<|>))
+import Data.Either (fromRight)
 
 eval :: Expr -> Either String Value
-eval (Value v) = Right v
-eval (UnOp op e) =
+eval (Value v _) = Right v
+eval (UnOp op e l) =
   eval e
     >>= case op of
       Negate -> neg
       Not -> not'
  where
-  neg v = TNum <$> expectNum negate v
+  neg v = TNum <$> expectNum negate l v
   not' v = TBool <$> expectBool not v
-eval (BinOp op e1 e2) = do
+eval (BinOp op e1 e2 l) = do
   v1 <- eval e1
   v2 <- eval e2
   operator v1 v2
  where
-  bopN op' wrap v1 v2 = wrap <$> (expectNum op' v1 >>= \f -> expectNum f v2)
+  bopN op' wrap v1 v2 = wrap <$> (expectNum op' l v1 >>= \f -> expectNum f l v2)
   bopB op' wrap v1 v2 = wrap <$> (expectBool op' v1 >>= \f -> expectBool f v2)
+  bopS op' wrap v1 v2 = wrap <$> (expectString op' v1 >>= \f -> expectString f v2)
 
-  eq (TNum x) = expectNum (== x)
+  eq (TNum x) = expectNum (== x) l
   eq (TBool b) = expectBool (== b)
   eq (TString s) = expectString (== s)
   eq TNil = expectNil True
 
-  eq' x y = TBool <$> eq x y
-  neq' x y = TBool . not <$> eq x y
+  eq' x y = either (const (Right False)) Right (eq x y)
+  eq'' x y = TBool <$> eq' x y
+  neq'' x y = TBool . not <$> eq' x y
+
+  fallibleDiv x y = do
+    x' <- expectNum id l x
+    y' <- expectNum id l y
+    if y' == 0.0
+      then Left "div by zero"
+      else Right . TNum $ x' / y'
 
   operator = case op of
     Add -> bopN (+) TNum
     Sub -> bopN (-) TNum
     Mul -> bopN (*) TNum
-    Div -> bopN (/) TNum
+    Div -> fallibleDiv
     Lt -> bopN (<) TBool
     Leq -> bopN (<=) TBool
     Gt -> bopN (>) TBool
     Geq -> bopN (>=) TBool
     Or -> bopB (||) TBool
     And -> bopB (&&) TBool
-    Eq -> eq'
-    Neq -> neq'
+    Eq -> eq''
+    Neq -> neq''
     Dot -> \_ _ -> Left ". operator not supported"
 eval _ = Left "expr not supported"
 
-expectNum :: (Double -> a) -> Value -> Either String a
-expectNum f (TNum x) = return $ f x
-expectNum _ v = Left $ "expected number, got: " ++ show v
+errorWith :: SourcePos -> String -> Either String a
+errorWith l s =
+  Left $
+    "line "
+      ++ show (unPos (sourceLine l))
+      ++ " col "
+      ++ show (unPos (sourceColumn l))
+      ++ ": "
+      ++ s
+
+expectNum :: (Double -> a) -> SourcePos -> Value -> Either String a
+expectNum f _ (TNum x) = return $ f x
+expectNum _ l v = errorWith l $ "expected number, got: " ++ show v
 
 expectBool :: (Bool -> a) -> Value -> Either String a
 expectBool f (TBool b) = return $ f b
